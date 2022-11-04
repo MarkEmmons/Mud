34d0e294dc92ebfd8f775dd0072ca03d52e10f75
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2022-11-04 17:07:59 -0500

    Move clap struct to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
 use clap::Parser;
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-
-	/// Name of a person to greet
-	#[arg(short, long)]
-	name: String,
-
-	/// Number of times to greet
-	#[arg(short, long, default_value_t = 1)]
-	count: u8,
-}
+use mud::args::MudOpts;
+use mud::packet::{
+	DnsPacket,
+	header::DnsHeader,
+	question::{DnsQuestion, encode_domain}
+};
 
 fn main() {
 
-	let args = Args::parse();
+	let args = MudOpts::parse();
 
-	for _ in 0..args.count {
+	let req = DnsPacket {
+		header: DnsHeader {
+			id: 1337,
+			qr: 0b1,
+			opcode: 0b0000,
+			aa: 0b0,
+			tc: 0b0,
+			rd: 0b1,
+			ra: 0b0,
+			z: 0b000,
+			rcode: 0b0000,
+			qd_count: 1,
+			an_count: 0,
+			ns_count: 0,
+			ar_count: 0,
+		},
+		question: DnsQuestion {
+			count: 0,
+			qname: encode_domain(&args.name),
+			qtype: 0x0001,
+			qclass: 0x0001,
+		}
+	};
 
-		println!("Hello, {}!", args.name);
-	}
+	println!("Server: {}", args.server);
+	println!("Name: {}", args.name);
+	println!("Type: {}", args.query_type);
+	println!("");
+	println!("Request: {:?}", req);
 }
