9f8d441049300e3959b3457a9b65ee0c958cba3f
Mark A Emmons <MarkAndreasEmmons@gmail.com>
2022-11-04 14:50:18 -0500

    Remove code from main, add first unit test to header.rs

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,4 @@
-use deku::prelude::*;
-
-use mud::packet::header::DnsHeader;
-
 fn main() {
 
-	let data: Vec<u8> = vec![
-
-		0b0000_0101,
-		0b0011_1001,	// ID
-
-		0b1000_0000 |	// QR
-		0b0000_0000 |	// OPCODE
-		0b0000_0000 |	// AA
-		0b0000_0000 |	// TC
-		0b0000_0001,	// RD
-
-		0b0000_0000 |	// RA
-		0b0000_0000 |	// Z
-		0b0000_0000,	// RCODE
-
-		0,			// QDCOUNT
-		1,			// QDCOUNT
-		0,			// ANCOUNT
-		0,			// ANCOUNT
-		0,			// NSCOUNT
-		0,			// NSCOUNT
-		0,			// ARCOUNT
-		0,			// ARCOUNT
-	];
-
-	let (_res, val) = DnsHeader::from_bytes((data.as_ref(), 0)).unwrap();
-
-
-	println!("{:?}", val);
+	println!("Hello, Mud!");
 }
--- src/packet/header.rs
+++ src/packet/header.rs
@@ -31,3 +31,51 @@ pub struct DnsHeader {
 	ns_count: u16,
 	ar_count: u16,
 }
+
+#[cfg(test)]
+mod tests {
+
+	use super::*;
+
+	#[test]
+	fn it_creates_a_header_from_bytes() {
+
+		let data: Vec<u8> = vec![
+
+			0b0000_0101,
+			0b0011_1001,	// ID
+
+			0b1000_0001,	// QR|OPCODE|AA|TC|RD
+
+			0b0000_0000,	// RA|Z|RCODE
+
+			0, 1,	// QDCOUNT
+			0, 0,	// ANCOUNT
+			0, 0,	// NSCOUNT
+			0, 0,	// ARCOUNT
+		];
+
+		let (_res, header) = DnsHeader::from_bytes(
+			(data.as_ref(), 0)
+		).unwrap();
+
+		assert_eq!(header, DnsHeader {
+			id: 1337,
+
+			qr: 0b1,
+			opcode: 0b0000,
+			aa: 0b0,
+			tc: 0b0,
+			rd: 0b1,
+
+			ra: 0b0,
+			z: 0b000,
+			rcode: 0b0000,
+
+			qd_count: 1,
+			an_count: 0,
+			ns_count: 0,
+			ar_count: 0,
+		});
+	}
+}
